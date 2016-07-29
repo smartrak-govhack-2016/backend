02525209185c08d6d6a610debbec3774a29e50bf
Lloyd Stockman <lloyd@smartrak.co.nz>
2016-07-30 10:53:54 +1200

    creating empty webapi solution.

---
+++ BicycleBackend.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BicycleBackend.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
