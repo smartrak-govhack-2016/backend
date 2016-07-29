d6aeaceea8bdc66105eaaa4dde478749728e318f
Lloyd Stockman <lloyd@smartrak.co.nz>
2016-07-30 11:00:51 +1200

    adding routing controller which will service requests from frontend

---
+++ BicycleBackend/Controllers/RoutingController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace BicycleBackend.Controllers
+{
+    public class RoutingController : ApiController
+    {
+        [HttpGet]
+        [Route("v1/route/{startlat},{startlon},{endlat},{endlon}")]
+        public IHttpActionResult GetRoute(double startLat, double startLon, double endLat, double endLon)
+        {
+            return Ok($"({startLat},{startLon}),({endLat},{endLon})");
+        }
+    }
+}
