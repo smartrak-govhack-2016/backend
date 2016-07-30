2fb795856b6170be1fe0de8414b2983a7d9b27e5
Lloyd Stockman <lloyd@smartrak.co.nz>
2016-07-30 12:18:20 +1200

    adding some sqlite db's from CAS with crash data in it. Adding a crash model

---
+++ BicycleBackend/Db/Crash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BicycleBackend.Db
+{
+    public class Crash
+    {
+        public int _Id { get; set; }
+        public DateTime CrashDate { get; set; }
+        public int CrashTime { get; set; }
+        public string ObjectsStruck { get; set; }
+        public string RoadWet { get; set; }
+        public string WthRa { get; set; }
+        public int SpdLim { get; set; }
+        public int CrashFatalCnt { get; set; }
+        public int CrashSevCnt { get; set; }
+        public int CrashMinCnt { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+}
