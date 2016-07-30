da2227fac90fd180dd14566679db23fed509481b
Lloyd Stockman <lloyd@smartrak.co.nz>
2016-07-30 14:55:22 +1200

    implementing router code by using kts a* implementation. starting building neighbor finding kind of, still need to do score.

---
+++ BicycleBackend/Routing/NeighborFinder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using OsmSharp.Collections.Tags;
+using OsmSharp.Osm;
+using OsmSharp.Osm.Streams.Filters;
+using OsmSharp.Osm.Xml.Streams;
+
+namespace BicycleBackend.Routing
+{
+    public interface INeighborFinder
+    {
+        IEnumerable<Segment> FindNeighbors(Segment segment);
+        Segment FindNearestNeighbor(double lat, double lon);
+    }
+    public class NeighborFinder : INeighborFinder
+    {
+        public string PathToMapData => $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\bicyclebicycle\\hamiltonmap";
+        //        public string PathToMapData => $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\bicyclebicycle\\new-zealand-latest.osm.pbf";
+        private List<string> ThingsWeThinkAreSwell => new List<string>()
+        {
+            "cycleway",
+            "cycleway:left",
+            "bicycle",
+            "highway",
+            "maxspeed",
+            "junction",
+            "roundabout"
+        };
+
+        public NeighborFinder()
+        {
+            using (var fileStream = new FileInfo(PathToMapData).OpenRead())
+            {
+                var source = new XmlOsmStreamSource(fileStream);
+                var filter = new OsmStreamFilterTagsFilter(TagsFilter);
+                filter.RegisterSource(source);
+                foreach (OsmGeo element in filter.ToArray())
+                {
+                    // add way
+                    // add nodes
+                    // add nodes to neighbor dictionary
+//                    element.
+//                    if (!element.Tags.Any(x => ThingsWeThinkAreSwell.Contains(x)))
+                        Debug.WriteLine(element.ToString());
+                }
+            }
+        }
+
+        private void TagsFilter(TagsCollectionBase collection)
+        {
+            foreach (var thing in ThingsWeThinkAreSwell)
+            {
+                if (collection.ContainsKey(thing))
+                {
+                    // do something maybe?
+                }
+            }
+        }
+
+        public IEnumerable<Segment> FindNeighbors(Segment segment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Segment FindNearestNeighbor(double lat, double lon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BicycleBackend/Routing/Router.cs
+++ BicycleBackend/Routing/Router.cs
@@ -1,29 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using Kts.AStar;
+using OsmSharp.Collections.Tags;
+using OsmSharp.Osm;
 using OsmSharp.Osm.PBF.Streams;
+using OsmSharp.Osm.Streams.Filters;
+using OsmSharp.Osm.Xml.Streams;
 
 namespace BicycleBackend.Routing
 {
     public class Router
     {
-        public string PathToMapData => $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\bicyclebicycle\\hamiltonmap";
+        private readonly INeighborFinder _neighborFinder;
 
-        public Router()
+        public Router(INeighborFinder neighborFinder)
         {
-            using (var fileStream = new FileInfo(PathToMapData).OpenRead())
-            {
-                var source = new PBFOsmStreamSource(fileStream);
-                foreach (var element in source)
-                {
-                    Console.WriteLine(element.ToString());
-                }
-            }
+            _neighborFinder = neighborFinder;
         }
 
         public IEnumerable<Segment> Route(double startLat, double startLon, double endLat, double endLon)
         {
-            return new Segment[0];
+            var start = _neighborFinder.FindNearestNeighbor(startLat, startLon);
+            var goal = _neighborFinder.FindNearestNeighbor(endLat, endLon);
+            return AStarUtilities.FindMinimalPath(start, goal, _neighborFinder.FindNeighbors, GetScore, segment => GetHScore(segment, goal)).Result;
+        }
+
+        private double GetScore(Segment segment, Segment otherSegment)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double GetHScore(Segment segment, Segment otherSegment)
+        {
+            throw new NotImplementedException();
         }
     }
 }
