96967f063fb833546dd48f02054a836b67887cf6
Lloyd Stockman <lloyd@smartrak.co.nz>
2016-07-30 13:23:26 +1200

    adding a bunch of things to do a bunch of things

--- BicycleBackend/Controllers/RoutingController.cs
+++ BicycleBackend/Controllers/RoutingController.cs
@@ -1,14 +1,32 @@
 ﻿using System.Web.Http;
+using BicycleBackend.Db;
 
 namespace BicycleBackend.Controllers
 {
     public class RoutingController : ApiController
     {
+        private readonly CrashContext _context;
+
+        public RoutingController()
+        {
+            _context = new CrashContext();
+        }
+
+        // segment
+        // name 
+        // danger level 1,2,3. Enum / text
         [HttpGet]
         [Route("v1/route/{startlat},{startlon},{endlat},{endlon}")]
         public IHttpActionResult GetRoute(double startLat, double startLon, double endLat, double endLon)
         {
             return Ok($"({startLat},{startLon}),({endLat},{endLon})");
+        }
+
+        [HttpGet]
+        [Route("v1/route/crashes")]
+        public IHttpActionResult GetCrashes()
+        {
+            return Ok(_context.GetCrashes());
         }
     }
 }
---
+++ BicycleBackend/Db/CrashContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace BicycleBackend.Db
+{
+    public class CrashContext : IDisposable
+    {
+        private readonly IDbConnection _conn;
+        private string DbPath => $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\bicyclebicycle\\crash.db";
+
+        public CrashContext()
+        {
+            _conn = new SQLiteConnection($"Data Source={DbPath};");
+            _conn.Open();
+        }
+
+        public IEnumerable<Crash> GetCrashes()
+        {
+            return _conn.Query<Crash>("select * from incidents");
+        }
+
+        public void Dispose()
+        {
+            _conn.Dispose();
+        }
+    }
+}
